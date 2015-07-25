e0f030f4e1dc00f26f49c28c5368fd7b3758903d
paphonbth <paphonb@icloud.com>
2015-07-25 15:42:16 +0700

    Tweaked status bar for >= API19 devices

--- app/src/main/java/com/kana2011/mcshop/HomeActivity.java
+++ app/src/main/java/com/kana2011/mcshop/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.kana2011.mcshop;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -49,6 +50,12 @@ public class HomeActivity extends ActionBarActivity {
         this.instance = this;
 
         setContentView(R.layout.activity_home);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
 
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/kana2011/mcshop/SettingsActivity.java
+++ app/src/main/java/com/kana2011/mcshop/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.kana2011.mcshop;
 
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class SettingsActivity extends ActionBarActivity {
@@ -16,6 +18,12 @@ public class SettingsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_settings);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
 
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
